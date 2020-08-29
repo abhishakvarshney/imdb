49f616bd602afe7bcc098b394488d759be92629d
Abhishak Varshney <abhishakvarshney@gmail.com>
2020-08-30 00:23:21 +0530

    complete functional code without css fixing

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ import json
 import ast
 import base64
 
-from django.contrib.auth.decorators import permission_required
+from django.core.exceptions import PermissionDenied
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login as login_method
 from django.contrib.auth import logout as logout_method
@@ -39,83 +39,93 @@ def login(request):
             request.session['user_id'] = user_id
             request.session['username'] = username
             redirect_url = u'/'
-            return HttpResponseRedirect(redirect_url)
+            return HttpResponseRedirect(reverse('movie:view'))
         else:
             print("Login failed for ->", username)
             msg = {'msg': 'Login failed. Authentication error. Please try again.'}
-
     return render(request, "login_new.html", {'msg': msg})
+
 
 def logout(request):
     logout_method(request)
-    # return redirect(reverse('login') + request.GET.get('next', ''))
+    return HttpResponseRedirect(reverse('movie:login'))
 
 
 
 # Create your views here.
-@api_view(["GET"])
 @parser_classes([JSONParser, MultiPartParser, FormParser])
 def view_movie(request):
+    perm = request.user.has_perm('movie.add_movies') and request.user.has_perm('movie.change_movies') and request.user.has_perm('movie.delete_movies')
+    user_active = request.user.is_authenticated
     if request.method == "GET":
         movie_list = models.Movies.view_movie()
-        return render(request, "view_movies.html", {'movie_list': movie_list, 'range': range(1, len(movie_list))})
+        return render(request, "view_movies.html", {'movie_list': movie_list, 'range': range(1, len(movie_list)), 'perm':perm, 'user_active':user_active})
 
 
 @parser_classes([JSONParser, MultiPartParser, FormParser])
-# @permission_required('auth.admin')
 def add_movie(request):
-    if request.method == "POST":
-        form = UploadJSONFileForm(request.POST, request.FILES)
-        json_data = []
-        if form.is_valid():
-            file = request.FILES['file']
-            json_data = file.read()
-            json_data = json.loads(json_data)
-            for movie_data in json_data:
-                movie = models.Movies.add_movie(**movie_data)
-        return HttpResponseRedirect(reverse('movie:view'))
-    form = UploadJSONFileForm()
-    return render(request, "add_movies.html", {'form': form})
+    print(":::", request.user.get_all_permissions())
+    print("LLLLL", request.user.is_superuser)
+    if request.user.has_perm('movie.add_movies'):
+        if request.method == "POST":
+            form = UploadJSONFileForm(request.POST, request.FILES)
+            json_data = []
+            if form.is_valid():
+                file = request.FILES['file']
+                json_data = file.read()
+                json_data = json.loads(json_data)
+                for movie_data in json_data:
+                    movie = models.Movies.add_movie(**movie_data)
+            return HttpResponseRedirect(reverse('movie:view'))
+        form = UploadJSONFileForm()
+        return render(request, "add_movies.html", {'form': form})
+    else:
+        raise PermissionDenied()
 
 
-# @api_view(["GET"])
 @parser_classes([JSONParser, MultiPartParser, FormParser])
-# @permission_required('auth.admin')
 def update_movie(request, data):
-    if request.method == "GET":
-        data = ast.literal_eval(data)
-        if len(request.GET) > 0:
-            movie_data = request.GET
-            movie_data._mutable = True
-            if movie_data.get('name') in  ['', None]:
-                movie_data['name'] = data.get('name', '')
-            if movie_data.get('director') in ['', None]:
-                movie_data['director'] = data.get('director', '')
-            if movie_data.get('imdb_score') in ['', None]:
-                movie_data['imdb_score'] = data.get('imdb_score', '')
-            if movie_data.get('popularity') in ['', None]:
-                movie_data['popularity'] = data.get('popularity', '')
-            if movie_data.get('genre') in ['', None]:
-                movie_data['genre'] = data.get('genre', '')
-            movie_update = models.Movies.update_movies(movie_data)
-        if isinstance(data, dict):
-            data = [data]
-        return render(request, "update_movies.html", {'movie_list': data})
+    if request.user.has_perm('movie.change_movies'):
+        if request.method == "GET":
+            data = ast.literal_eval(data)
+            if len(request.GET) > 0:
+                movie_data = request.GET
+                movie_data._mutable = True
+                if movie_data.get('name') in  ['', None]:
+                    movie_data['name'] = data.get('name', '')
+                if movie_data.get('director') in ['', None]:
+                    movie_data['director'] = data.get('director', '')
+                if movie_data.get('imdb_score') in ['', None]:
+                    movie_data['imdb_score'] = data.get('imdb_score', '')
+                if movie_data.get('popularity') in ['', None]:
+                    movie_data['popularity'] = data.get('popularity', '')
+                if movie_data.get('genre') in ['', None]:
+                    movie_data['genre'] = data.get('genre', '')
+                movie_update = models.Movies.update_movies(movie_data)
+            if isinstance(data, dict):
+                data = [data]
+            return render(request, "update_movies.html", {'movie_list': data})
+    else:
+        raise PermissionDenied()
 
 
-# @api_view(["POST"])
 @parser_classes([JSONParser, MultiPartParser, FormParser])
-# @permission_required('auth.admin')
 def delete_movie(request, movie_name):
-    if movie_name is not None:
-        is_deleted = models.Movies.delete_movies(movie_name)
-    if is_deleted:
+    if request.user.has_perm('movie.delete_movies'):
+        if movie_name is not None:
+            is_deleted = models.Movies.delete_movies(movie_name)
+        if is_deleted:
+            return HttpResponseRedirect(reverse('movie:view'))
         return HttpResponseRedirect(reverse('movie:view'))
-    return HttpResponseRedirect(reverse('movie:view'))
+    else:
+        raise PermissionDenied()
+
 
 
 @parser_classes([JSONParser, MultiPartParser, FormParser])
 def search_movie(request):
+    perm = request.user.has_perm('movie.add_movies') and request.user.has_perm('movie.change_movies') and request.user.has_perm('movie.delete_movies')
+    user_active = request.user.is_authenticated
     movie_name = request.GET.get('q', '')
     movie_list = models.Movies.search_movie(movie_name)
-    return render(request, "view_movies.html", {'movie_list': movie_list, 'range': range(1, len(movie_list))})
+    return render(request, "view_movies.html", {'movie_list': movie_list, 'range': range(1, len(movie_list)), 'perm':perm, 'user_active':user_active})
